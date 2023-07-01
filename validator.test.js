2541f243051242a37d64f7522f1a019f8ec49fe5
Avinav-kashyap04 <avinav04kashyap@gmail.com>
2023-07-01 19:22:27 +0530

    A teammable app project

---
+++ validator.test.js
@@ -0,0 +1,39 @@
+const { isInvalidEmail, isEmptyPayload } = require('../validator')
+
+test('valid email', function() {
+    const testPayload = {
+        name: "test name",
+        email: "test.email@example.com",
+        interests: "testing"
+    }
+    const result = isInvalidEmail(testPayload)
+    expect(result).toBe(false)
+})
+
+test('invalid email', function() {
+    const testPayload = {
+        name: "test name",
+        email: "test.email",
+        interests: "testing"
+    }
+    const result = isInvalidEmail(testPayload)
+    expect(result).toBe(true)
+})
+
+test('empty payload', function() {
+    const testPayload = {}
+    const result = isEmptyPayload(testPayload)
+    expect(result).toBe(true)
+    //passing an empty payload
+})
+
+test('non-empty payload', function() {
+    const testPayload = {
+        name: "test name",
+        email: "test.email@example.com",
+        interests: "testing"
+    }
+    const result = isEmptyPayload(testPayload)
+    expect(result).toBe(false)
+    //here payload is not empty
+})
